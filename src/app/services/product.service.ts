45cba9a56be2b69fe8d43ce6cc06a3cb2888b3b6
goceconev <goceconev@live.com>
2023-10-31 16:22:35 +0100

    Finished product-service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,13 +9,13 @@ export class ProductService {
   private products: Product[] = [
     { id: 1, name: 'Product 1', description: 'Description 1', price: 10 },
     { id: 2, name: 'Product 2', description: 'Description 2', price: 15 },
-    { id: 2, name: 'Product 3', description: 'Description 3', price: 20 },
-    { id: 2, name: 'Product 4', description: 'Description 4', price: 30 },
-    { id: 2, name: 'Product 5', description: 'Description 5', price: 15 },
-    { id: 2, name: 'Product 6', description: 'Description 6', price: 40 },
-    { id: 2, name: 'Product 7', description: 'Description 7', price: 20 },
-    { id: 2, name: 'Product 8', description: 'Description 8', price: 15 },
-    { id: 2, name: 'Product 9', description: 'Description 9', price: 25 },
+    { id: 3, name: 'Product 3', description: 'Description 3', price: 20 },
+    { id: 4, name: 'Product 4', description: 'Description 4', price: 30 },
+    { id: 5, name: 'Product 5', description: 'Description 5', price: 15 },
+    { id: 6, name: 'Product 6', description: 'Description 6', price: 40 },
+    { id: 7, name: 'Product 7', description: 'Description 7', price: 20 },
+    { id: 8, name: 'Product 8', description: 'Description 8', price: 15 },
+    { id: 9, name: 'Product 9', description: 'Description 9', price: 25 },
 
   ];
 
